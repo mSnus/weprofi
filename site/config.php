cd051d4b6d215588e66f8ea4ff400ca365e4718d
mSnus <snus@sitebuilding.ru>
2021-05-06 14:10:57 +0300

    merged with PW

---
+++ site/config.php
@@ -0,0 +1,147 @@
+<?php namespace ProcessWire;
+
+/**
+ * ProcessWire Configuration File
+ *
+ * Site-specific configuration for ProcessWire
+ * 
+ * Please see the file /wire/config.php which contains all configuration options you may
+ * specify here. Simply copy any of the configuration options from that file and paste
+ * them into this file in order to modify them. 
+ * 
+ * SECURITY NOTICE
+ * In non-dedicated environments, you should lock down the permissions of this file so
+ * that it cannot be seen by other users on the system. For more information, please
+ * see the config.php section at: https://processwire.com/docs/security/file-permissions/
+ * 
+ * This file is licensed under the MIT license
+ * https://processwire.com/about/license/mit/
+ *
+ * ProcessWire 3.x, Copyright 2019 by Ryan Cramer
+ * https://processwire.com
+ *
+ */
+
+if(!defined("PROCESSWIRE")) die();
+
+/*** SITE CONFIG *************************************************************************/
+
+/** @var Config $config */
+
+/**
+ * Prepend template file
+ *
+ * PHP file in /site/templates/ that will be loaded before each page's template file.
+ * Example: _init.php
+ *
+ * @var string
+ *
+ */
+$config->prependTemplateFile = '_init.php';
+
+/**
+ * Append template file
+ *
+ * PHP file in /site/templates/ that will be loaded after each page's template file.
+ * Example: _main.php
+ *
+ * @var string
+ *
+ */
+$config->appendTemplateFile = '_main.php';
+
+/**
+ * Allow core API variables to also be accessed as functions?
+ *
+ * Recommended. This enables API varibles like $pages to also be accessed as pages(),
+ * as an example. And so on for most other core variables.
+ *
+ * Benefits are better type hinting, always in scope, and potentially shorter API calls.
+ * See the file /wire/core/FunctionsAPI.php for details on these functions.
+ *
+ * @var bool
+ *
+ */
+$config->useFunctionsAPI = true;
+
+
+/*** INSTALLER CONFIG ********************************************************************/
+
+/**
+ * Installer: Database Configuration
+ * 
+ */
+$config->dbHost = 'localhost';
+$config->dbName = 'procwire';
+$config->dbUser = 'procwire';
+$config->dbPass = 'jq65O2CWmmM5nOlo9gJBKcCaWAtqBq';
+$config->dbPort = '3306';
+
+/**
+ * Installer: User Authentication Salt 
+ * 
+ * This value was randomly generated for your system on 2021/05/05.
+ * This should be kept as private as a password and never stored in the database.
+ * Must be retained if you migrate your site from one server to another.
+ * Do not change this value, or user passwords will no longer work.
+ * 
+ */
+$config->userAuthSalt = '4f310f989c9314cc188d3ecf6022161ffb725ead'; 
+
+/**
+ * Installer: Table Salt (General Purpose) 
+ * 
+ * Use this rather than userAuthSalt when a hashing salt is needed for non user 
+ * authentication purposes. Like with userAuthSalt, you should never change 
+ * this value or it may break internal system comparisons that use it. 
+ * 
+ */
+$config->tableSalt = 'c54611b3e48546d2a23eb6b30b81e37e4f9295fb'; 
+
+/**
+ * Installer: File Permission Configuration
+ * 
+ */
+$config->chmodDir = '0755'; // permission for directories created by ProcessWire
+$config->chmodFile = '0644'; // permission for files created by ProcessWire 
+
+/**
+ * Installer: Time zone setting
+ * 
+ */
+$config->timezone = 'Europe/Moscow';
+
+/**
+ * Installer: Admin theme
+ * 
+ */
+$config->defaultAdminTheme = 'AdminThemeUikit';
+
+/**
+ * Installer: Unix timestamp of date/time installed
+ * 
+ * This is used to detect which when certain behaviors must be backwards compatible.
+ * Please leave this value as-is.
+ * 
+ */
+$config->installed = 1620230048;
+
+
+/**
+ * Installer: HTTP Hosts Whitelist
+ * 
+ */
+$config->httpHosts = array('a-bot.online');
+
+
+/**
+ * Installer: Debug mode?
+ * 
+ * When debug mode is true, errors and exceptions are visible. 
+ * When false, they are not visible except to superuser and in logs. 
+ * Should be true for development sites and false for live/production sites. 
+ * 
+ */
+$config->debug = true;
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
