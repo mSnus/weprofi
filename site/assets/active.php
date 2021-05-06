12472f363a32b39fe93248a67bfb88533d459551
mSnus <snus@sitebuilding.ru>
2021-05-06 14:10:57 +0300

    merged with PW

---
+++ site/assets/active.php
@@ -0,0 +1,1 @@
+<?php // Created by ProcessWire - Do not delete this file. The existence of this file indicates the site is confirmed active and first-time use errors may be suppressed. Installed at: [/home/admin/web/v620214.hosted-by-vdsina.ru/public_html/pw/]
